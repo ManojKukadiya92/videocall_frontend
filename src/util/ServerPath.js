563786829bb426a1db7a43b12651aa037528b74e
ManojKukadiya92 <63141010+ManojKukadiya92@users.noreply.github.com>
2023-09-04 11:15:39 +0530

    add

--- src/util/ServerPath.js
+++ src/util/ServerPath.js
@@ -1,4 +1,4 @@
 export const baseURL = "https://octopus-app-f26t8.ondigitalocean.app/";
 //export const baseURL = "https://videoapp.skytouchinfotech.com";
 //export const baseURL = "http://localhost:5000";
-export const key = "Skytouch"; //same as backend & android
+export const key = "bb6GAYAad7SdezsYpGeHqEg7BU6gLwEn"; //same as backend & android
