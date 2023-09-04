527cc40282087368f6f946e4ec5360a885b1a4fc
ManojKukadiya92 <63141010+ManojKukadiya92@users.noreply.github.com>
2023-09-04 19:20:18 +0530

    init

--- src/util/ServerPath.js
+++ src/util/ServerPath.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://octopus-app-f26t8.ondigitalocean.app/";
+export const baseURL = "https://monkfish-app-pxnl3.ondigitalocean.app/";
 //export const baseURL = "https://videoapp.skytouchinfotech.com";
-//export const baseURL = "http://localhost:5000";
+//export const baseURL = "http://localhost:50001";
 export const key = "bb6GAYAad7SdezsYpGeHqEg7BU6gLwEn"; //same as backend & android
